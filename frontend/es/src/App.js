6b3011c8289851f44be728548756975e4b4fc0d3
João Teixeira <joaocostateixeira@ua.pt>
2020-10-27 11:19:07 +0000

    frontend cenas

--- frontend/es/src/App.js
+++ frontend/es/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router> 
   );
+}
+
+function Home() {
+  return (
+    <div id="page-wrapper">
+
+      <div id="header">
+          <nav id="nav">
+            <ul>
+              <li class="current"><a href="index.html">Home</a></li>
+              <li> <a href="index.html">Home</a></li>
+              <li> <a href="index.html">Home</a></li>
+              <li> <a href="index.html">Home</a></li>
+              <li> <a href="index.html">Home</a></li>
+            </ul>
+          </nav>
+
+      </div>
+      </div>
+
+    );
 }
 
 export default App;
